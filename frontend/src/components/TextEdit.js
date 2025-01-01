c55bce2f86a290f913dad208267ecd51ccfbc17d
Anita-Cahya-Pratiwi <129023785+Anita-Cahya-Pratiwi@users.noreply.github.com>
2025-01-01 18:19:13 +0700

    Add files via upload

---
+++ frontend/src/components/TextEdit.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './textedit.css';
+import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:4000');
+
+class TextEdit extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    };
+  }
+
+  handleChange = (event) => {
+    const text = event.target.value;
+    this.setState({ text });
+    socket.emit('text-change', text); //mengirim
+  };
+
+  componentDidMount() {
+    socket.on('text-update', (text) => {
+      this.setState({ text });  //perbarui
+    });
+  }
+
+  render() {
+    return (
+      <div className="text-edit-container">
+        <h1>Selamat Datang di JalaKata</h1>
+        <h4>"Aplikasi Dokumen Kolaboratif"</h4>
+        <textarea
+          rows="10"
+          cols="50"
+          placeholder="Tuliskan teks disini"
+          value={this.state.text}
+          onChange={this.handleChange} 
+        ></textarea>
+      </div>
+    );
+  }
+}
+
+export default TextEdit;
