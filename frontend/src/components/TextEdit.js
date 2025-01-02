579d4d9df98569b9193b6db584cfc753c3eb0864
Anita-Cahya-Pratiwi <129023785+Anita-Cahya-Pratiwi@users.noreply.github.com>
2025-01-02 21:58:41 +0700

    Update TextEdit.js

--- frontend/src/components/TextEdit.js
+++ frontend/src/components/TextEdit.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './textedit.css';
 import { io } from 'socket.io-client';
 
-const socket = io('https://serverjk.vercel.app/');
+const socket = io('https://jalakataid.vercel.app/');
 
 class TextEdit extends Component {
   constructor(props) {
